058482d267d733a4476f15dfdf5364c29480da6b
laylaabokaf <abokafl@post.bgu.ac.il>
2023-08-26 13:30:19 +0300

    TaskQueueApplicationMultiThreadingClass added , sectionB.py Done .

---
+++ TaskQueueApplicationMultiThreading.py
@@ -0,0 +1,77 @@
+import queue
+import threading
+import time
+import os
+import subprocess
+import concurrent.futures
+
+class TaskQueueApplicationMultiThreadingClass:
+    def __init__(self, tasks_folder, queue_length, max_concurrent_tasks, log_file, event):
+        self.tasks_folder = tasks_folder
+        self.executed_task_names = []
+        self.program_start_time = time.time()
+        self.task_queue = queue.Queue(queue_length)
+        self.max_concurrent_tasks = max_concurrent_tasks
+        self.stop_app = event
+        self.log_file = log_file
+        self.log_lock = threading.Lock()
+        self.file_access_lock = threading.Lock()
+
+    def task_function(self, task_id):
+        script_path = f'{self.tasks_folder}/{task_id}.py'
+        start_time = time.time() - self.program_start_time
+        try:
+            subprocess.run(['python', script_path], check=True)
+            end_time = time.time() - self.program_start_time
+            thread_id = threading.get_ident()  # Get the current thread ID
+            self.log_task_times(task_id, start_time, end_time, thread_id)
+        except subprocess.CalledProcessError:
+            self.log_task_times(task_id, 0, -1, threading.get_ident())
+
+    def add_task(self, task_id):
+        self.task_queue.put(task_id)
+
+    def process_task(self, task_id):
+        self.task_function(task_id)
+        self.task_queue.task_done()
+
+    def process_tasks(self):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_concurrent_tasks) as executor:
+            while not self.stop_app.is_set():
+                task_id = self.task_queue.get()
+                executor.submit(self.process_task, task_id)
+
+    def watch_folder(self):
+        while not self.stop_app.is_set():
+            for filename in os.listdir(self.tasks_folder):
+                if filename.endswith('.py'):
+                    task_id = os.path.splitext(filename)[0]
+                    if task_id not in self.executed_task_names:
+                        with self.file_access_lock:
+                            self.executed_task_names.append(task_id)
+                            self.add_task(task_id)
+            time.sleep(5)
+
+    def log_task_times(self, task_name, start_time, end_time, thread_id):
+        with self.log_lock:
+            with open(self.log_file, 'a') as record_file:
+                if end_time == -1:
+                    record_file.write(f"Thread {thread_id}: Task '{task_name}' Failed\n")
+                else:
+                    record_file.write(f"Thread {thread_id}: Task '{task_name}' Completed: Started at {start_time}, Ended at {end_time}\n")
+
+    def clear_log_file(self):
+        open(self.log_file, 'w').close()
+
+    def run(self):
+        task_thread = threading.Thread(target=self.process_tasks)
+        task_thread.daemon = True
+
+        watch_folder_thread = threading.Thread(target=self.watch_folder)
+        watch_folder_thread.daemon = True
+
+        task_thread.start()
+        watch_folder_thread.start()
+
+        watch_folder_thread.join()
+        self.task_queue.join()
---
+++ sectionB.py
@@ -0,0 +1,38 @@
+import queue
+import threading
+import time
+import os
+import subprocess
+import configparser
+from TaskQueueApplicationMultiThreading import TaskQueueApplicationMultiThreadingClass
+
+
+# Read configuration from config.ini file
+config_obj = configparser.ConfigParser()
+config_obj.read("configfile.ini")
+param = config_obj["sectionB"]
+
+# Extract configuration parameters
+queue_length = int(param["max_queue_length"])
+log_file = param["log_task_file_name"]
+tasks_folder = param["tasks_folder"]
+max_concurrent_tasks = param["max_concurrent_tasks"]
+
+# Create a threading event to potentially stop the running threads
+stop_app = threading.Event()
+
+# Initialize the TaskQueueApplicationMultiThreadingClass
+app = TaskQueueApplicationMultiThreadingClass(
+    tasks_folder=tasks_folder,
+    queue_length=queue_length,
+    max_concurrent_tasks=int(max_concurrent_tasks),
+    log_file=log_file,
+    event=stop_app
+)
+
+try:
+    # Run the application
+    app.run()
+except KeyboardInterrupt:
+    print("Stopping the application...")
+    stop_app.set()  # Set event to stop the application
